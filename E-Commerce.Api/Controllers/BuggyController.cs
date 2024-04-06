7f92b22cd629b04ed136382af23cf36b590c0754
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-04-06 23:57:35 +0300

    Projeye Exception Handling eklendi middleware olarak exceptionu environment'e göre result olarak dönüyorum.

---
+++ E-Commerce.Api/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce.Infrastructure;
+using E_Commerce.Service.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Api.Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+        private readonly AppDbContext _context;
+
+        public BuggyController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("not-found")]
+        public  IActionResult GetNotFoundRequest()
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id ==5);
+            if(product == null) 
+                return NotFound(new ApiResponse(404));
+            return Ok();
+        }
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == 5);
+
+            var result = product.ToString();
+            return Ok();
+        }
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ E-Commerce.Api/MiddleWares/ExceptionMiddleWare.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce.Service.Errors;
+using System.Text.Json;
+
+namespace E_Commerce.Api.MiddleWares
+{
+    public class ExceptionMiddleWare
+    {
+        readonly RequestDelegate _next;
+        readonly ILogger<ExceptionMiddleWare> _logger;
+        readonly IHostEnvironment _environment;
+        public ExceptionMiddleWare(RequestDelegate next,ILogger<ExceptionMiddleWare> logger,IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _environment = env;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                int statusCode = StatusCodes.Status500InternalServerError;
+                _logger.LogError(ex,ex.Message);
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = statusCode;
+                var response = _environment.IsDevelopment()
+                    ? new ApiException(statusCode, ex.Message, ex.StackTrace.ToString())
+                    : new ApiException(statusCode);
+
+                var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response,option);
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+
+    }
+}
